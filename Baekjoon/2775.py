7892ab7f512370ecddc5b1c2ba77add0d00c4d17
702criticcal <wnstn1549@gmail.com>
2020-08-15 22:24:46 +0900

    부녀회장이 됱 테야 풀이

--- Baekjoon/2775.py
+++ Baekjoon/2775.py
@@ -32,4 +32,15 @@
 #     # count+=1
 # print(sum)
 
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1, n+1)]
+    for _ in range(k):
+        for j in range(1,n):
+            people[j] += people[j-1]
+    print(people[-1])
+
+
+
 
