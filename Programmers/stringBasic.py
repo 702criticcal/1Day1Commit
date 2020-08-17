0dddfb7ee909385e21cd96cbd80b25660b93e346
702criticcal <wnstn1549@gmail.com>
2020-08-17 12:04:23 +0900

    문자열 다루기 기본 isdigit() 활용

--- Programmers/stringBasic.py
+++ Programmers/stringBasic.py
@@ -1,9 +1,2 @@
 def solution(s):
-    try:
-        int(s)
-        if len(s) == 4 or len(s) == 6:
-            return True
-        else:
-            return False
-    except:
-        return False
+    return s.isdigit() and len(s) in (4,6)
