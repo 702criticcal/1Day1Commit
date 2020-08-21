bcf590f55dcd7cce6679fa9bb4e753cdc22a5f54
702criticcal <wnstn1549@gmail.com>
2020-08-21 14:32:52 +0900

    문자열을 정수로 바꾸기 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.

---
+++ Programmers/stringToNum.py
@@ -0,0 +1,10 @@
+def solution(s):
+    if s[0] == '-':
+        return int(s[1:]) * -1
+    elif s[0] == '+':
+        return int(s[1:])
+    else:
+        return int(s)
+
+print(solution('1234'))
+print(solution('-1234'))
