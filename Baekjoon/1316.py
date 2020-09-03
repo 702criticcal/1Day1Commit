c8ea91e46c0458d097966b78917d807405e23648
702criticcal <wnstn1549@gmail.com>
2020-09-03 09:49:13 +0900

    그룹 단어 체커

--- Baekjoon/1316.py
+++ Baekjoon/1316.py
@@ -1,5 +1,18 @@
-num = int(input())
-str_list = []
-for i in range(0, num):
-    str_list.append(input())
-print(str_list)
+N = int(input())
+count = 0
+for i in range(N):
+    alpabetList = []
+    s = input()
+    alpabetList.append(s[0])
+    if len(s) == 1:
+        count += 1
+    else:
+        for j in range(1, len(s)):
+            if s[j - 1] != s[j]:
+                if s[j] in alpabetList:
+                    break
+                else:
+                    alpabetList.append(s[j])
+            if j == len(s) - 1:
+                count += 1
+print(count)
