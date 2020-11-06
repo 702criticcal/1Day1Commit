30845371a3b9ea850d6f0e00451e3416347c3801
702criticcal <wnstn1549@gmail.com>
2020-11-06 16:48:11 +0900

    타겟 넘버 2차 풀이
    2차 풀이 및 파일명 변경.

--- Programmers/타겟 넘버.py
+++ Programmers/타겟 넘버.py
@@ -16,3 +16,25 @@ def solution(numbers, target):
 
     dfs(0, numbers, target, 0)
     return answer
+
+
+# 2020.11.06 두 번째 풀이.
+answer = 0
+
+
+def dfs(i, numbers, target, value):
+    global answer
+    if i == len(numbers) and value == target:
+        answer += 1
+        return
+    if i == len(numbers):
+        return
+
+    dfs(i + 1, numbers, target, value + numbers[i])
+    dfs(i + 1, numbers, target, value - numbers[i])
+
+
+def solution(numbers, target):
+    global answer
+    dfs(0, numbers, target, 0)
+    return answer
