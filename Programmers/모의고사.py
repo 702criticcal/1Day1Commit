3c25107fe601789a104c7dee2eab964206609c46
702criticcal <wnstn1549@gmail.com>
2020-11-07 20:32:25 +0900

    모의고사
    1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.

---
+++ Programmers/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    answer = []
+    correct = [0, 0, 0]
+
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    for i in range(len(answers)):
+        if answers[i] == first[i % len(first)]:
+            correct[0] += 1
+        if answers[i] == second[i % len(second)]:
+            correct[1] += 1
+        if answers[i] == third[i % len(third)]:
+            correct[2] += 1
+
+    for i, v in enumerate(correct):
+        if v == max(correct):
+            answer.append(i + 1)
+
+    return answer
