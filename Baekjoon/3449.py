8a9c531ee57a973a6221ce1b86f20cc071d9127a
702criticcal <wnstn1549@gmail.com>
2020-11-22 21:03:05 +0900

    해밍 거리
    해밍 거리란 두 숫자의 서로 다른 자리수의 개수이다. 두 이진수가 주어졌을 때, 해밍 거리를 계산하는 프로그램을 작성하시오.

---
+++ Baekjoon/3449.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    a = input()
+    b = input()
+    cnt = 0
+
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            cnt += 1
+
+    print(f'Hamming distance is {cnt}.')
