9328a3439ff619797816cf2c1f04e2d952c56261
702criticcal <wnstn1549@gmail.com>
2020-11-25 17:50:18 +0900

    등굣길 2차 풀이
    2차 풀이 및 파일명 변경.

--- Programmers/등굣길.py
+++ Programmers/등굣길.py
@@ -17,3 +17,19 @@ def solution(m, n, puddles):
 
 
 print(solution(4, 3, [[2, 2]]))
+
+
+# 2020.11.25 2차 풀이.
+def solution(m, n, puddles):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[1][1] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1:
+                continue
+            if [j, i] in puddles:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+    return dp[n][m] % 1000000007
